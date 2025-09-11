23be5ae4ed1ebaf7d0ea0fe279498db3a826c829
Tokui-js <alan5jaquez@gmail.com>
2025-09-11 16:34:06 -0400

    Class - 09/11/25

---
+++ Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour
+{
+    public float timer;
+    public float gameTimeLimit =30f;
+    public float timeLeft; 
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //amount of time that has passed since th elast frame
+        timer += Time.deltaTime;
+    }
+   void FixedUpdate()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/WASDController.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    public float Speed = 10f;
+    public Rigidbody2D myRB;
+    public bool jumped = false;
+    public bool grounded = false;
+    public float jumpForce = 100f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space) && grounded)
+        {
+            jumped = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        //let's start writing a WASD controller
+        Vector3 velocity = Vector3.zero;
+        /*if (Input.GetKey(KeyCode.W))
+        {
+            velocity.y = Speed;
+
+        }*/
+        if (Input.GetKey(KeyCode.A))
+        {
+            velocity.x = -Speed;
+        }
+        /*if (Input.GetKey(KeyCode.S))
+        {
+            velocity.y = -Speed;
+        }*/
+        if (Input.GetKey(KeyCode.D))
+        {
+            velocity.x = Speed;
+        }
+        myRB.AddForce (velocity);
+        if (jumped)
+        {
+            Jump();
+            jumped = false;
+        }
+    }
+
+    void Jump()
+    {
+        myRB.AddForce(Vector3.up * jumpForce);
+
+    }
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        Debug.Log("stopped collinding with" + collision.gameObject);
+            grounded = false;
+    }
+}
