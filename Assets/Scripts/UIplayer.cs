bd332ccd86eb163ba88cf35b9870ff1a01038fd4
Tokui-js <alan5jaquez@gmail.com>
2025-10-09 17:08:46 -0400

    Changes-in-class/10/09-5:08 pm

--- Assets/Scripts/UIplayer.cs
+++ Assets/Scripts/UIplayer.cs
@@ -1,16 +1,29 @@
 using UnityEngine;
+using TMPro;
 
 public class UIplayer : MonoBehaviour
 {
+
+    public string playerName;
+
+    public GameObject nameField;
+
+    public TMP_InputField inputField;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        DontDestroyOnLoad(this.gameObject);
+        inputField = nameField.GetComponent<TMP_InputField>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void chnageName()
+    {
+        playerName = inputField.text;
+        Debug.Log("name change called");
     }
 }
