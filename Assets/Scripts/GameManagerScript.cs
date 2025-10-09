45392626bc4413fe4b59b5e80f718da8be819399
Tokui-js <alan5jaquez@gmail.com>
2025-10-09 13:15:34 -0400

    Changes-10/09/25

--- Assets/Scripts/GameManagerScript.cs
+++ Assets/Scripts/GameManagerScript.cs
@@ -3,12 +3,12 @@ using UnityEngine;
 public class GameManagerScript : MonoBehaviour
 {
     public float timer;
-    public float gameTimeLimit =30f;
-    public float timeLeft; 
+    public float gameTimeLimit = 30f;
+    public float timeLeft;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -17,8 +17,9 @@ public class GameManagerScript : MonoBehaviour
         //amount of time that has passed since th elast frame
         timer += Time.deltaTime;
     }
-   void FixedUpdate()
+    void FixedUpdate()
     {
-        
+
     }
+    
 }
