5a228338b7f9a66f2e0ae6232876b9d277572d92
Tokui-js <alan5jaquez@gmail.com>
2025-11-06 14:21:26 -0500

    Update ButtonHelper.cs

--- Assets/Scripts/ButtonHelper.cs
+++ Assets/Scripts/ButtonHelper.cs
@@ -3,14 +3,30 @@ using UnityEngine.SceneManagement;
 
 public class ButtonHelper : MonoBehaviour
 {
+    string
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public UIplayer myPlayer;
+
     void Start()
     {
-        
+        myPlayer = UIplayer._playerInstance;
+        if (myPlayer == null)
+        {
+            Debug.LogError("No player instance found in scene");
+        }
     }
 
-   public void ChangeScene(string newScene)
+   public void ChangeScene(string LivingRoom)
     {
-        SceneManager.LoadScene(newScene);
+        SceneManager.LoadScene(LivingRoom);
+    }
+    public void AddToInventory(string itemName)
+    {
+        for (int i = 0; myPlayer.inventory < i; )
+        {
+
+        }
     }
 }
